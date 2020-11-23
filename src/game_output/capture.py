94706a07c9469477e9f9e5aad6e4b8034875fb2f
arthurPignet <arthur.pignet@mines-paristech.fr>
2020-11-23 22:41:30 +0100

    Some update on the capture class

--- src/game_output/capture.py
+++ src/game_output/capture.py
@@ -3,7 +3,7 @@ from multiprocessing import Queue, Process
 import numpy as np
 import cv2
 import win32gui
-from PIL import ImageGrab, Image
+from PIL import ImageGrab
 
 # four character code object for video writer
 
@@ -14,15 +14,15 @@ fourcc = cv2.VideoWriter_fourcc(*'XVID')
 # video writer object
 
 class GameCapture:
-    screen_name = 'Celeste'
+    window_name = 'Celeste'
 
     def __init__(self, save=False, **kwargs):
         self.save = save
         if self.save:
             self.file_out = cv2.VideoWriter(kwargs.get('path', "./output.avi"), fourcc, 5.0, (1366, 768))
         self.output_queue = Queue()
-        self.celeste_window = self._get_screens()
-        self.process = Process(target=self.game_capture, args=(self.output_queue,self.celeste_window))
+        self.celeste_window_id = win32gui.FindWindow(None, self.window_name)
+        self.process = Process(target=self.game_capture, args=(self.output_queue, self.celeste_window_id))
 
     def start(self):
         self.process.start()
@@ -50,28 +50,14 @@ class GameCapture:
             if cv2.waitKey(1) == 27:  # useful to quit the while loop
                 break
 
-    def _get_screens(self):
-        def enum_cb(hwnd, result_list):
-            result_list.append((hwnd, win32gui.GetWindowText(hwnd)))
-
-        # wait for the program to start initially.
-        winlist = []
-        win32gui.EnumWindows(enum_cb, winlist)
-        screens = [(hwnd, title) for hwnd, title in winlist if self.screen_name == title]
-        while len(screens) == 0:
-            screens = [(hwnd, title) for hwnd, title in winlist if self.screen_name == title]
-            win32gui.EnumWindows(enum_cb, winlist)
-
-        return screens[0][0]
-
     @staticmethod
-    def game_capture(queue_out, window):
-        window_box = win32gui.GetWindowRect(window)
+    def game_capture(queue_out, window_id):
+        window_box = win32gui.GetWindowPlacement(window_id)[-1]
         while True:
             # capture computer screen
-            img = ImageGrab.grab(bbox=window_box)
+            img = ImageGrab.grab(bbox=window_box, all_screens=True)
             # convert image to numpy array
             img_np = np.array(img)
             # convert color space from BGR to RGB
-            #frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+            # frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
             queue_out.put(img_np)
